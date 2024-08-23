e2928ef5d17711438d92a62113e4933d3bdb4fd7
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2024-08-23 01:01:46 -0400

    test pushing failed test

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,7 @@ const { sum } = require('./math.js');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 5 + 6 to equal 11', () => {
+  expect(sum(5, 66)).toBe(11);
+})
